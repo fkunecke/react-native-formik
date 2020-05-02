ba084c77355de33a08ec1caa1b8cef33783fd52d
Forest Kunecke <fkunecke@gmail.com>
2020-05-02 15:37:11 -0400

    removed dependencies on withpickervalues

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import withFormik from "./src/withFormik";
 import withFormikControl from "./src/withFormikControl";
 import withInputTypeProps from "./src/withInputTypeProps";
 import withTouched from "./src/withTouched";
-// import withPickerValues from "./src/withPickerValues";
-// import KeyboardModal from "./src/withPickerValues/KeyboardModal";
 import makeReactNativeField from "./src/makeReactNativeField";
 import {
   withNextInputAutoFocusForm,
@@ -40,6 +38,4 @@ export {
   withTouched,
   withNextInputAutoFocusForm,
   withNextInputAutoFocusInput,
-  // withPickerValues,
-  // KeyboardModal
 };
